04ea656c14cc927434b608f2c6edd6596e5cefe5
Josh <joshua.mcswain@mecklenburgcountync.gov>
2021-12-01 20:02:08 -0500

    pushing changs to index.html

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,4 +1,4 @@
 // create variable to hold db connection
 let db;
 // establish a connection to IndexedDB database called 'Budjet-Tracker' 
-const request = indexedDB.open('dudget-tracker', 1);
+const request = indexedDB.open('budget-tracker', 1);
