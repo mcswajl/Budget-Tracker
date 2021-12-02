64b0d69fdcf607cb5ba2bfc2cadf46974ad5bef3
Josh <joshua.mcswain@mecklenburgcountync.gov>
2021-12-02 12:44:00 -0500

    pushing changs

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 const express = require("express");
-// const logger = require("morgan");
+const logger = require("morgan");
 const mongoose = require("mongoose");
-// const compression = require("compression");
-const router = require('./routes/api.js')
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-const { transaction } = require('./models/transaction');
+app.use(logger("dev"));
 
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
-  useFindAndModify: false,
+app.use(express.static("public"));
+
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useFindAndModify: false
 });
 
-app.use(router)
+// routes
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
