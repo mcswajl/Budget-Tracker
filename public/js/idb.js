d922daf4c671191600a57bae2778700225886e7c
Josh <joshua.mcswain@mecklenburgcountync.gov>
2021-12-03 10:23:38 -0500

    added upload function to my idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,19 +1,12 @@
-// create variable to hold db connection
 let db;
-// establish a connection to IndexedDB database called 'Budjet-Tracker' 
 const request = indexedDB.open('budget', 1);
 
-// this event will emit if the database version changes
 request.onupgradeneeded = function(event) {
 
-    // save a reference to the database
     const db = event.target.result;
-
-    // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts
     db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 
-// upon a successful
 request.onsuccess = function(event) {
 
     // when db is successfully created with its object store or simply established a connection, save reference to db in global variable
@@ -21,7 +14,7 @@ request.onsuccess = function(event) {
 
     // check if app is online, if yes run uploadTransaction() function to send all local db data to api
     if (navigator.onLine) {
-        // todo: uploadTransaction();
+        uploadTransaction();
     }
 };
 
@@ -30,13 +23,10 @@ request.onerror = function(event) {
     console.log(event.target.errorCode);
 };
 
-// This function will be executed if we attempt to submit a new transaction and there's no internet connection
 function saveRecord(record) {
 
-    // open a new transaction with the database with read and write permissions
     const transaction = db.transaction(['new_transaction'], 'readwrite');
 
-    // access the object store for `new_transaction`
     const  budgetObjectStore = transaction.objectStore('new_transaction');
 
     // add record to your store with add method
@@ -72,10 +62,8 @@ function uploadTransaction() {
                         throw new Error(serverResponse);
                     }
 
-                    // open one more transaction
                     const transaction = db.transaction(['new_transaction'], 'readwrite');
 
-                    // access the new_transaction object store
                     const budgetObjectStore = transaction.objectStore('new_transaction');
 
                     // clear all items in your store
